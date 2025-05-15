02830d8695f6d0a924eae0cb441da4d6750b4834
Muhammad-Bilal-Malik <bilalyadali786@gmail.com>
2025-05-16 00:11:00 +0500

    added new component reagarding trading section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import Footer from "./Components/Footer";
+import Hero from "./Components/Hero";
 import Login from "./Components/Login";
 import Navbar from "./Components/Navbar";
 import Signup from "./Components/Signup";
 import { Routes, Route } from "react-router-dom";
+import Trendings from "./Components/Trendings";
 function App() {
   return (
     <>
-      <Navbar />
+      <h1>jjjj</h1>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar /> <Hero />
+              <Trendings />
+              <Footer />
+            </>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-      <Footer />
     </>
   );
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import BGRectanggle from "../Assets/Images/BGRectangle.svg";
+import heroimg from "../Assets/Images/heroimg.svg";
+const Hero = () => {
+  return (
+    <div className="hero w-full">
+      <img
+        className="w-screen absolute -top-5"
+        src={BGRectanggle}
+        alt="heroBackgroundImage"
+      />
+      <div className=" relative flex justify-evenly items-center  ">
+        <div className="w-[474px] space-y-2 mb-36 tracking-tighter">
+          <p className="font-bold text-[40px] text-[#162F42] ">
+            Trade On Politics & More
+          </p>
+          <p className="text-[#4F4F4F]">
+            The largest social prediction market.
+          </p>
+          <p className="text-[#4F4F4F]">
+            Get real-time odds on politics, tech, and sports. Win cash prizes
+            for your predictions!
+          </p>
+          <button className="w-[100%] bg-[#00D094] text-white rounded-md py-2.5">
+            Start predicting
+          </button>
+        </div>
+        <img className="h-[415px]" src={heroimg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
